b1e9cad0c6bd2a46eaf5b4c07b5fee3b5360c39e
Yash Nandwani <yashnandwani202@gmail.com>
2026-07-01 02:09:43 +0530

    Connected Flask login with MySQL database

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+app.secret_key = "smart_lab_secret_key"
+
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = "1234"
+app.config["MYSQL_DB"] = "smart_lab_db"
+
+mysql = MySQL(app)
+
 
 @app.route("/")
 def home():
@@ -9,14 +19,22 @@ def home():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-
     if request.method == "POST":
-
         email = request.form["email"]
         password = request.form["password"]
 
-        print("Email:", email)
-        print("Password:", password)
+        cursor = mysql.connection.cursor()
+        cursor.execute(
+            "SELECT * FROM users WHERE email = %s AND password = %s",
+            (email, password)
+        )
+        user = cursor.fetchone()
+        cursor.close()
+
+        if user:
+            return "Login Successful"
+        else:
+            flash("Invalid email or password")
 
     return render_template("login.html")
 
