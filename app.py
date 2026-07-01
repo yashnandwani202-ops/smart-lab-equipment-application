228780ec0d589ac9f200115056d55143d884e1d3
Yash Nandwani <yashnandwani202@gmail.com>
2026-07-01 15:42:15 +0530

    Implemented role-based login and dashboards

--- app.py
+++ app.py
@@ -32,11 +32,33 @@ def login():
         cursor.close()
 
         if user:
-            return "Login Successful"
+            role = user[4]
+
+            if role == "student":
+                return redirect(url_for("student_dashboard"))
+            elif role == "faculty":
+                return redirect(url_for("faculty_dashboard"))
+            elif role == "admin":
+                return redirect(url_for("admin_dashboard"))
         else:
             flash("Invalid email or password")
 
     return render_template("login.html")
+
+
+@app.route("/student")
+def student_dashboard():
+    return render_template("student_dashboard.html")
+
+
+@app.route("/faculty")
+def faculty_dashboard():
+    return render_template("faculty_dashboard.html")
+
+
+@app.route("/admin")
+def admin_dashboard():
+    return render_template("admin_dashboard.html")
 
 
 if __name__ == "__main__":
